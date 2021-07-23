372a4a14440f6128913faefac259b360ea8837a1
lindemberg9 <berg@armagedom.localdomain>
2021-07-23 00:29:23 -0300

    variaveis

---
+++ variaveis/js/js.js
@@ -0,0 +1,16 @@
+// variavel string
+var nome;
+
+nome = "Paulo";
+// variavel inteiro
+var idade = 25;
+
+// vareavel float ou flutuante
+var pretencaoSalarial = 1700.22;
+
+// variavel booleano
+var valeRefeicao = true;
+
+/*
+    Fim de codigo
+*/
