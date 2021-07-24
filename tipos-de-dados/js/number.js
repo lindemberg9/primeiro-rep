7a8ac600f5ab21df02986218a6ef347bde91a087
Lindemberg9 <berg@armagedom.localdomain>
2021-07-24 01:53:30 -0300

    tipo numerico

---
+++ tipos-de-dados/js/number.js
@@ -0,0 +1,21 @@
+var idade = 28;
+console.log(idade);
+console.log(typeof idade);
+
+var precoFeijao = 2.89;
+console.log(precoFeijao);
+
+var valor = +"23" + 23; // "+" na frente converte para numero
+var numero = -38;
+console.log(valor);
+console.log(numero);
+console.log(numero + valor);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+console.log(valor);
+console.log(valor.toString(10));
+console.log(valor.toString(2));
+console.log(valor.toString(8));
+console.log(valor.toString(16));
