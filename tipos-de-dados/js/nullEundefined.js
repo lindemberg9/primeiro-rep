5ac814ddd79aae438445a936f827fa8bed92e080
lindemberg9 <berg@armagedom.localdomain>
2021-07-24 22:15:43 -0300

    mull and undefined

---
+++ tipos-de-dados/js/nullEundefined.js
@@ -0,0 +1,18 @@
+var nome;
+console.log(nome);
+nome = "Berg";
+console.log(nome);
+
+nome = undefined; // Já mais uzar
+nome - null; // liberar espaço na memoria
+
+console.log("undefined == null", undefined == null);
+console.log("undefined", typeof undefined);
+console.log("null", typeof null);
+console.log("undefined === null", undefined === null);
+
+// var idade;
+var pessoa = {nome: "Berg"};
+
+// console.log(pessoa.idade);
+console.log(pessoa.nome);
