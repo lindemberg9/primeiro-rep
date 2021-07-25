8bdc87133ed1cba550dd803b8dcff7d8f8bf68d6
lindemberg9 <berg@armagedom.localdomain>
2021-07-24 23:57:00 -0300

    array

---
+++ tipos-de-dados/js/array.js
@@ -0,0 +1,13 @@
+// Literal
+var produtos = [];
+var produtos2 = ['Produto A', 'Produto B', 'Produto C'];
+var valores = ['Pedro', 'Maria', 45, true, {nome: 'Antonio'}];
+
+// Constutor 
+var array = new Array();
+var array2 = new Array('A', 23, false);
+
+// Acessando
+console.log(produtos2[2]);
+console.log(valores[4].nome);
+console.log(array2[1]);
