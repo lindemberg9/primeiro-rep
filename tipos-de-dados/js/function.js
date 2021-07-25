78b2f2b335db3f9cc9b8b61402f34b38bbf7d861
lindemberg9 <berg@armagedom.localdomain>
2021-07-25 00:05:27 -0300

    function

---
+++ tipos-de-dados/js/function.js
@@ -0,0 +1,11 @@
+function exibirNoConsole(mensagem) {
+    console.log(mensagem);
+}
+
+exibirNoConsole('Pedreiro Charles');
+
+function soma(v1, v2) {
+    return v1 + v2;
+}
+
+console.log(soma(2, 5));
