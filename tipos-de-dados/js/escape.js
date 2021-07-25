5bfa7c69613ac682af667e5f9e3eafb9940dff2c
lindemberg9 <berg@armagedom.localdomain>
2021-07-24 23:09:22 -0300

    escape

---
+++ tipos-de-dados/js/escape.js
@@ -0,0 +1,9 @@
+console.log("texto \n teto");
+console.log("\u00A9");
+console.log("\xA9");
+console.log('text \'teto');
+console.log("texto \"teto");
+console.log("texto \\teto");
+
+var corp = "©"
+console.log(corp.charCodeAt());
