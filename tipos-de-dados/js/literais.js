1cfbf6c2a78ee6cf7536e0989958de692fc9c047
lindemberg9 <berg@armagedom.localdomain>
2021-07-24 22:49:49 -0300

    literais

---
+++ tipos-de-dados/js/literais.js
@@ -0,0 +1,19 @@
+// Literais: São valores de daddos que aparecem dirertamente nas declarações
+var nomeCurso = "1 - JavaScript";
+var nomeCurso2 = '2 - JavaScript';
+var nomeCurso3 = new String('3 - JavaScript'); // Não literal
+console.log(nomeCurso);
+console.log(nomeCurso2);
+console.log(nomeCurso3);
+
+var idade = 33;
+var preco = 13.5;
+var numero = new Number(35); // Não literal
+console.log(idade);
+console.log(preco);
+console.log(numero);
+
+var bollean = true;
+var bollean2 = new Boolean(true); // Não literal
+console.log(bollean);
+console.log(bollean2.toString());
