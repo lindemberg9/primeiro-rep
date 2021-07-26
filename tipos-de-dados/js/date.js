5cf0f870fa12d752cc1764b20f68c2579758fecd
lindemberg9 <berg@armagedom.localdomain>
2021-07-25 22:24:00 -0300

    date

---
+++ tipos-de-dados/js/date.js
@@ -0,0 +1,14 @@
+var data = new Date();
+console.log(data);
+
+var dataString = new Date("2019-10-22");
+console.log(dataString.getDate()+1);
+console.log(dataString.getDay());
+console.log(dataString.getMonth()+1);
+console.log(dataString.getMonth()+1);
+console.log(dataString.getTime());
+console.log(dataString.getHours());
+console.log(data.getMinutes());
+
+var dataParam = new Date(2021,04,21);
+console.log(dataParam);
