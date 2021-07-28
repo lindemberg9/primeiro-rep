ee26cbac38b244d8f71c14e2f4a4f6d5f4de2827
lindemberg9 <berg@armagedom.localdomain>
2021-07-27 23:49:41 -0300

    converso-tipos

---
+++ escopo-outros/js/conversao-tipos-dado.js
@@ -0,0 +1,10 @@
+// conversão automatica
+console.log(23 + ' trinta e cinco');
+console.log(55 + "5");
+console.log(10 / "2");
+console.log("3" * "2");
+console.log("3" / {valor:15});
+
+// conversão de forma explicita por construtor
+
+console.log(Number("23"));
