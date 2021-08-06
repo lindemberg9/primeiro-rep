2ca15aab54695c3b9db32da428a9f54055f739f0
lindemberg9 <berg@armagedom.localdomain>
2021-08-06 07:59:18 -0300

    operadores aritimeticos / adição

---
+++ operadores/aritmeticos/adicao.js
@@ -0,0 +1,14 @@
+// Adição
+
+var soma = 20 + 7;
+console.log("20 + 7 = ",soma);
+console.log("20 + 20");
+console.log(20 + "20");
+console.log("Berg " + "Lima");
+console.log(20 + 20);
+var numStr = "55";
+console.log(numStr, typeof numStr);
+console.log(typeof +numStr);
+console.log(true + false + true);
+console.log(10 + null);
+console.log(10 + undefined);
