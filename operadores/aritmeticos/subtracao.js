5a4227d6fc63fd6298cc381e1f4763493562448c
lindemberg9 <berg@armagedom.localdomain>
2021-08-06 08:23:42 -0300

    subtração

---
+++ operadores/aritmeticos/subtracao.js
@@ -0,0 +1,11 @@
+// Subtração
+var sub = 550 - 320;
+console.log("550 - 320 = ", sub);
+console.log("-120 - 30 = ", -120 - 30);
+console.log("-120 +30 = ", -120 +30);
+console.log("+30 = ", -30);
+console.log("16 - true = ", 16 - true);
+var strNum = "22";
+console.log("-(strNum = '22')", -strNum);
+console.log("typeof - (strNum = '22')", typeof -strNum);
+console.log("0.2 + 0.2 = ", 0.2 + 0.2)
