8886689af715feee54398454a6790868c4f94972
lindemberg9 <berg@armagedom.localdomain>
2021-08-06 11:37:59 -0300

    incremento e decremento

---
+++ operadores/aritmeticos/increDecremento.js
@@ -0,0 +1,15 @@
+// Incremento
+var incremento = 10;
+incremento++;
+console.log("1 - incremento 10 (++)", incremento);
+console.log("2 - ++incremento ", ++incremento);
+console.log("3 - incremento ", incremento);
+
+console.log("####################################")
+
+// Decremento
+var decremento = 16;
+decremento--;
+console.log("1 - decremento 10 (--)", decremento);
+console.log("2 - ++decremento ", --decremento);
+console.log("3 - decremento ", decremento);
