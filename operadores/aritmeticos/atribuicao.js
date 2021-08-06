0ba632d382edf35d09b69f2d28ca146e4eb32302
lindemberg9 <berg@armagedom.localdomain>
2021-08-06 15:40:38 -0300

    operadores de atribuição

---
+++ operadores/aritmeticos/atribuicao.js
@@ -0,0 +1,45 @@
+// // Operador '='
+// var numero = 10;
+// var numero2 = 1;
+// var nome = "Pedro";
+
+// numero = numero = numero2;
+// //console.log(numero);
+
+// // Operador +=
+// var numero3 = 10;
+// //numero3 = numero3 + 1;
+// //numero3++;
+// numero3 += 20;
+// console.log(numero3);
+// nome += " Silva";
+// console.log("Nome completo: ", nome);
+// nome += true;
+// console.log(nome);
+// numero3 += true;
+// console.log(numero3);
+
+// Operador -=
+var sub = 30;
+sub -= 20;
+console.log(sub);
+
+// Operador *=
+var mult = 2;
+mult *= 6;
+console.log(mult);
+
+// Operador div /=
+var div = 5;
+div /= 2;
+console.log(div);
+
+// Operador %=
+var mod = 15;
+mod %= 2;
+console.log(mod);
+
+// Operador **=
+var exp = 2;
+exp **= 4;
+console.log(exp);
