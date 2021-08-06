735dfd853471c7d26da62b409a5bb1fb43b2684a
lindemberg9 <berg@armagedom.localdomain>
2021-08-06 09:19:36 -0300

    multiplicação

---
+++ operadores/aritmeticos/multiplicacao.js
@@ -0,0 +1,12 @@
+// Multiplicação
+
+var mult = 5 * 20;
+console.log("5 * 20 = ", mult);
+console.log('7 * "3" = ', 7 * "3");
+console.log('7 * "tres" = ',  7 * "tres");
+console.log("Precedencia: ", 3 + 3 * 2);
+console.log("precedencia: () ", (3 + 3) * 2);
+console.log("3 * 0 = ", 3 * 0);
+console.log("0 * 3 = ", 0 * 3);
+console.log("3 * -2 = ", 3 * -2);
+console.log("2 * true = ", 2 * true);
