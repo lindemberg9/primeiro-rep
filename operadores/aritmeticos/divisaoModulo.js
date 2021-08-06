d32750ef2339b91ef8a10d2f5dc08cf183c9b031
lindemberg9 <berg@armagedom.localdomain>
2021-08-06 10:01:59 -0300

    divisão e modulo

---
+++ operadores/aritmeticos/divisaoModulo.js
@@ -0,0 +1,13 @@
+var div = 10 / 2;
+console.log("10 / 2 = ", div);
+console.log("10 / 0 = ", 10 / 0);
+console.log("-10 / 0 = ", -10 / 0);
+console.log("0 / 10 = ", 0 / 10);
+console.log("3 / 4 = ", 3 / 4);
+console.log("infinit / infinit = ", Infinity / Infinity);
+console.log("10 / '2' = ", 10 / '2');
+console.log("10 / 'dois' = ", 10 / 'dois');
+
+// ###### Modulo ######
+var mod = 9 % 2;
+console.log("9 % 2 = ", mod);
