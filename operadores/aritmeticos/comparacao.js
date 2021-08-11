2f2ba5b2f2afc1a6e820bfefaf1c7bfbf4c8e08e
lindemberg9 <berg@armagedom.localdomain>
2021-08-10 23:50:19 -0300

    comparação

---
+++ operadores/aritmeticos/comparacao.js
@@ -0,0 +1,13 @@
+var op1 = 10;
+var op2 = 20;
+console.log("1 - ", op1 == op2);
+console.log("2 - ", 10 == "10");
+console.log("3 - ", 10 === "10");
+console.log("4 - ", 1 == true);
+console.log("5 - ", 1 === true);
+console.log("6 - ", 2 != 2);
+console.log("7 - ", 2 !== "2");
+console.log("8 - ", 2 > 1);
+console.log("9 - ", 2 < 1);
+console.log("10 - ", 8 <= 8);
+console.log("10 - ", 8 >= 9);
