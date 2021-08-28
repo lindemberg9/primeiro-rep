8f0e63eabdea6487e48dcc7736bb1fc78ec1c6e3
lindemberg9 <berg@armagedom.localdomain>
2021-08-27 23:32:53 -0300

    switch

---
+++ 07-estruturas-logicas/switch.js
@@ -0,0 +1,16 @@
+var str = 'JS';
+
+switch (str) {
+    case "js":
+        console.log("js");
+        break;
+    case "Js":
+        console.log("Js");
+        break;
+    case "JS":
+        console.log("JS");
+        break;
+    default:
+        console.log("Não encontrado");
+        break;
+}
