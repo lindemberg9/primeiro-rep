a0c102acf35f7279a876d0658fd9079d2ec57cee
lindemberg9 <berg@armagedom.localdomain>
2021-08-27 21:49:39 -0300

    if else

---
+++ 07-estruturas-logicas/if-else.js
@@ -0,0 +1,17 @@
+var condicao = true;
+
+if(condicao) console.log("1 if");
+
+if(condicao)
+console.log("2 if");
+else 
+console.log("else");
+
+condicao = false; 
+if(condicao){
+    console.log("1 bloco");
+    console.log("1 bloco");
+} else {
+    console.log("2 bloco");
+    console.log("2 bloco");
+}
