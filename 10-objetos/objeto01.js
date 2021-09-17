d92440f3dff5f660adba69813b5560b15a8b4905
lindemberg9 <berg@armagedom.localdomain>
2021-09-16 23:52:28 -0300

    literal

--- 10-objetos/objeto01.js
+++ 10-objetos/objeto01.js
@@ -22,3 +22,33 @@ var obj05 = new RegExp(/teste/);
 var obj06 = new Function("parametroNome", "return 'Olá ' + parametroNome + '!'");
 
 console.log(obj06("Fram"));
+
+// Criar obj literal
+var objetoPessoa = {
+    nome: "João Ferrira",
+    cpf: 12345678923,
+    dataNascimento: new Date(1982, 3, 11),
+    ativo: true,
+    "teste prop": "teste",
+    contatos: [11111111, 22222222, 33333333],
+    endereco: {rua: "Rua B", numero: 123, cep: 12333000},
+    saudacao: function(){
+        return "Olá, me chamo " + this.nome + "!";
+    }
+}
+
+console.log(objetoPessoa);
+
+console.log("Nome:", objetoPessoa.nome);
+console.log("CPF:", objetoPessoa.cpf);
+console.log("Nascimento:", objetoPessoa.dataNascimento);
+console.log("Ativo:", objetoPessoa.ativo == true ? "Sim" : "Não");
+console.log("Teste:", objetoPessoa["teste prop"]);
+console.log("Contato:", objetoPessoa.contatos[0]);
+for (var contatos of objetoPessoa.contatos){
+    console.log("Contatos:", contatos);
+}
+for (var key in objetoPessoa.endereco){
+    console.log(key, objetoPessoa.endereco[key]);
+}
+console.log(objetoPessoa.saudacao());
